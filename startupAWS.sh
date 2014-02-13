092768d3c77342a0bd23e24c6df610f19f6fe0dc
Ubuntu <ubuntu@ip-10-16-20-114.ec2.internal>
2014-02-13 00:35:55 +0000

    updated script for install CUDA 5 and all DWPACLEC2 dependencies

--- startupAWS.sh
+++ startupAWS.sh
@@ -1,36 +1,46 @@
 #/bin/bash
+echo Thank you Sunjay Dhama -- www.sunjaydhama.com
+echo this will do everything for you for aws
 sudo apt-get update
-sudo apt-get install gcc
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/cuda-repo-ubuntu1204_5.5-0_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1204_5.5-0_amd64.deb
 sudo apt-get update
-sudo apt-get install cuda
-sudo apt-get install opencl-headers python-pip python-dev python-numpy python-mako
-wget https://pypi.python.org/packages/source/p/pyopencl/pyopencl-2013.1.tar.gz#md5=c506e4ec5bc56ad85bf005ec40d4783b
-tar -vxzf pyopencl-2013.1.tar.gz
-cd pyopencl-2013.1
-sudo python setup.py install
-
-
-echo adding PATH and LD_LIBRARY path to bashrc
-echo -e 'export PATH=/usr/local/cuda-5.5/bin:$PATH' >> ~/.bashrc
-echo -e 'export LD_LIBRARY_PATH=/usr/local/cuda-5.5/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+wget https://raw.github.com/AmmarkoV/MyScripts/master/Setup/install_cuda5
+chmod 755 install_cuda5 
+./install_cuda5 
+exec bash
+ sudo apt-get update
+ sudo apt-get upgrade
+ sudo ldconfig /usr/local/cuda/lib64
+echo running deviceQuery
 cd /usr/local/cuda-5.5/samples/1_Utilities/deviceQuery
 sudo make
-sudo ./deviceQuery
-
-echo generating ssh key for github
-cd ~/.ssh
-ssh-keygen -t rsa -C "AWS GPU"
-echo add to github ssh keys
-cat id_rsa.pub
-echo press any key when you have finished
-read abcd
-
+sudo /usr/local/cuda/samples/bin/linux/release/deviceQuery 
+echo one more simple test
+cd /usr/local/cuda/samples/0_Simple/clock
+sudo make
+/usr/local/cuda/samples/bin/linux/release/clock 
 cd ~
-echo cloning my repo
-git clone -b my-br git@github.com:sdsunjay/DWPACLEC2.git
-
+echo CUDA installed
+echo generating ssh key for github
+ssh-keygen -t rsa -C "AWS CG1 GPU"
+echo add to github ssh keys
+cat ~/.ssh/id_rsa.pub
+sudo apt-get install git
+echo cloning DWPACLEC2
+git clone git@github.com:sdsunjay/DWPACLEC2.git
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install sqlite3 libsqlite3-dev
+sudo apt-get install openssl
+sudo apt-get install libssl-dev
 cd ~/DWPACLEC2/src/slave
-make
-cd ~/DWPACLEC2/bin/linux/release/wpa-crack-s 7373
+sudo make
+echo starting
+~/DWPACLEC2/bin/linux/release/wpa-crack-s 7373
+
+#optional for other stuff you may be doing, but not necessary
+#sudo apt-get install opencl-headers python-pip python-dev python-numpy python-mako
+#wget https://pypi.python.org/packages/source/p/pyopencl/pyopencl-2013.1.tar.gz#md5=c506e4ec5bc56ad85bf005ec40d4783b
+#tar -vxzf pyopencl-2013.1.tar.gz
+#cd pyopencl-2013.1
+#sudo python setup.py install
+
+
