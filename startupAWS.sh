e459ad733d7ca37b909c8211ccfc3a5581362e7b
Sunjay Dhama <sdsunjay@users.noreply.github.com>
2015-10-28 00:20:32 -0700

    Updated startupAWS.sh
    Updated startupAWS.sh with the user choosing whether to install CUDA 5 or not (untested)

--- startupAWS.sh
+++ startupAWS.sh
@@ -1,24 +1,33 @@
 #/bin/bash
 echo Thank you Sunjay Dhama -- www.sunjaydhama.com
-echo this will do everything for you for aws
+echo this script will do everything for you for AWS
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install libmysqlclient-dev
-sudo apt-get install sqlite3 libsqlite3-dev
-sudo apt-get install openssl
-sudo apt-get install libssl-dev
-sudo apt-get install mysql-client-core-5.5
+sudo apt-get install libmysqlclient-dev sqlite3 libsqlite3-dev install openssl libssl-dev mysql-client-core-5.5
+
+function install_cuda_5 {
+               echo Installing CUDA 5
+               ./install_cuda5
+               exec bash
+               sudo ldconfig /usr/local/cuda/lib64
+               echo running deviceQuery
+               cd /usr/local/cuda-5.5/samples/1_Utilities/deviceQuery
+               sudo make
+               sudo /usr/local/cuda/samples/bin/linux/release/deviceQuery 
+               echo CUDA installed
+                echo May have to reboot if it did not install correctly
+           }
 #optional for other stuff you may be doing, but not necessary
 #sudo apt-get install opencl-headers python-pip python-dev python-numpy python-mako
-./install_cuda5 
-exec bash
-sudo ldconfig /usr/local/cuda/lib64
-echo running deviceQuery
-cd /usr/local/cuda-5.5/samples/1_Utilities/deviceQuery
-sudo make
-sudo /usr/local/cuda/samples/bin/linux/release/deviceQuery 
-echo CUDA installed
-echo May have to reboot if not
+while true; do
+read -p "Do you wish to install CUDA 5?" yn
+    case $yn in
+        [Yy]* ) install_cude_5;  break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 #echo generating ssh key for github
 #ssh-keygen -t rsa -C "AWS CG1 GPU"
 #echo add to github ssh keys
@@ -28,7 +37,7 @@ echo May have to reboot if not
 #git clone git@github.com:sdsunjay/DWPACLEC2.git
 cd ~/DWPACLEC2/src/slave
 sudo make
-echo starting
+echo starting on port 7373
 sudo ~/DWPACLEC2/bin/linux/release/wpa-crack-s 7373
 
 #optional for other stuff you may be doing, but not necessary
